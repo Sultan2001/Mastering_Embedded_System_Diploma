2eaeb41d0652135c876c7ffc6299b8159a117699
mohamed sultan <mohammedsoltan170@gmail.com>
2023-08-18 06:08:46 +0300

    Reverse string

---
+++ C_programing/Homework_3/Strings/EX3/main.c
@@ -0,0 +1,40 @@
+/*
+ * main.c
+ *
+ *  Created on: ١٨‏/٠٨‏/٢٠٢٣
+ *      Author: mohamedsultan
+ */
+
+#include "stdio.h"
+void main()
+{
+	char string[100];
+
+
+	int i=0 ;
+
+	printf("Enter a string : ");
+	fflush(stdin); fflush(stdout);
+	for(i=0; i<100; i++)
+	{
+		scanf("%c",&string[i]);
+		if ('\n'==string[i])
+		{
+			string[i]='\0';
+
+			break;
+		}
+	}
+	int y=0;
+
+
+	for(y=i-1 ; y>=0; y--)
+	{
+		printf("%c",string[y]);
+	}
+
+
+
+
+
+}
