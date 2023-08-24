b704b1c2434d68067a6d5741a1e2b75b0c61c14a
mohamed sultan <mohammedsoltan170@gmail.com>
2023-08-24 23:48:42 +0300

    Calculate the power of a Number Using Recursion

---
+++ C_programing/Homework_4/EX4/main.c
@@ -0,0 +1,35 @@
+/*
+ * main.c
+ *
+ *  Created on: ٢٤‏/٠٨‏/٢٠٢٣
+ *      Author: mohamedsultan
+ */
+
+
+#include "stdio.h"
+int power(int num , int p);
+int main()
+{
+
+int base,pow;
+printf("Enter base number : ");
+fflush(stdin); fflush(stdout);
+scanf("%d",&base);
+printf("Enter power number (positive integer): ");
+fflush(stdin); fflush(stdout);
+scanf("%d",&pow);
+printf("%d",power(base,pow));
+
+	return 0;
+}
+int power(int num , int p)
+{
+	if (p==0)
+	{
+		return 1;
+	}
+	else
+	{
+		return power(num,p-1)*num;
+	}
+}
